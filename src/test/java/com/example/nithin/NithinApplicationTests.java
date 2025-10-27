3e55d08639af8dfdbb8e79cd821fecc62966f09a
rubannithinabraha <rubannithinabraham.26ec@licet.ac.in>
2025-10-27 22:34:34 +0530

    Add files via upload

---
+++ src/test/java/com/example/nithin/NithinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nithin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NithinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
